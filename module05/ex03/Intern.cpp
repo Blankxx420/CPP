e98e80c908ddd98cc6664de36b95ea5ce098659e
Blankxx420 <briceguichou.dev@gmail.com>
2025-04-30 23:56:29 +0200

    ex03 started

--- module05/ex03/Intern.cpp
+++ module05/ex03/Intern.cpp
@@ -6,20 +6,15 @@
 /*   By: brguicho <brguicho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 10:20:36 by brguicho          #+#    #+#             */
-/*   Updated: 2025/04/29 10:52:52 by brguicho         ###   ########.fr       */
+/*   Updated: 2025/04/30 23:38:07 by brguicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include  "Intern.hpp"
 
-Intern::Intern(): _form_to_create(NULL), _target(NULL)
+Intern::Intern()
 {
 	std::cout << "Intern default constructor called" << std::endl;
-}
-
-Intern::Intern(std::string form_to_create, std::string target): _form_to_create(form_to_create), _target(target)
-{
-	std::cout << "Intern constructor called" << std::endl;
 }
 
 Intern::Intern(const Intern &copied)
@@ -28,11 +23,10 @@ Intern::Intern(const Intern &copied)
 	*this = copied;
 }
 
-Intern& Intern::operator=(const Intern& copied)
+Intern& Intern::operator=(const Intern & copied)
 {
+	(void)copied;
 	std::cout << "Intern assignation operator called" << std::endl;
-	this->_target = copied.get_Target();
-	this->_form_to_create = copied.get_Form_To_Create();
 	return *this;
 }
 
@@ -41,34 +35,33 @@ Intern::~Intern()
 	std::cout << "Intern deconstructor called" << std::endl;
 }
 
-std::string Intern::get_Target() const
-{
-	return (this->_target);
-}
-
-std::string Intern::get_Form_To_Create() const
-{
-	return (this->_form_to_create);
-}
-
 AForm * Intern::makeForm(std::string form_to_create, std::string target)
 {
-	
+	int i = 0;
+	AForm *form;
+	std::string form_name[3] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+	while (i < 4 && form_name[i].compare(form_to_create) != 0)
+		i++;
+	switch (i)
+	{
+		case 0:
+			form = new ShrubberyCreationForm(target);
+			break;
+		case 1:
+			form = new RobotomyRequestForm(target);
+			break;
+		case 2:
+			form = new PresidentialPardonForm(target);
+			break;
+	}
+	if (form)
+		std::cout << "Intern creates " << form->getName() << std::endl;
+	else
+		throw (Intern::UnknowFormException());
+	return (form);
 }
 
-char const	*Intern::EmptyFormNameException::what(void) const throw()
+char const	*Intern::UnknowFormException::what() const throw()
 {
-	return ("Name of Form is not given");
-}
-
-char const	*Intern::EmptytTargetException::what(void) const throw()
-{
-	return ("Name of target is not given");
-}
-
-char const	*Intern::UnknowFormException::what(std::string form_name) const throw()
-{
-	std::string msg = form_name + " Unknow form name";
-	char const * error = msg.c_str();
-	return (error);
+	return ("Unknow form name");
 }
--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -6,75 +6,65 @@
 /*   By: brguicho <brguicho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 08:51:41 by brguicho          #+#    #+#             */
-/*   Updated: 2025/04/29 09:46:28 by brguicho         ###   ########.fr       */
+/*   Updated: 2025/04/30 23:42:33 by brguicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
-#include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 
-int main(void)
+int	main(void)
 {
+
+	// Constructors
+	std::cout << std::endl;
+	std::cout << "CONSTRUCTING:" << std::endl;
+	std::cout << "----------------------------" << std::endl;
+
+	Intern	intern;
+	AForm	*robo_form;
+	AForm	*shrub_form;
+	AForm	*president_form;
+	AForm	*unknown_form;
+	std::cout << std::endl << std::endl;
+	//-----------------------------------------------------
+
+
+	std::cout << std::endl;
+	std::cout << "INTERN TESTS:" << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	// Robo:
+	robo_form = intern.makeForm("robotomy request", "robo");
+	std::cout << *robo_form << " was just created " << std::endl << std::endl;
+
+	// President:
+	president_form = intern.makeForm("presidential pardon", "president");
+	std::cout << *president_form << " was just created " << std::endl << std::endl;
+
+	// Shrub:
+	shrub_form = intern.makeForm("shrubbery creation", "shrub");
+	std::cout << *shrub_form << " was just created " << std::endl << std::endl;
+
+	// Unknown:
 	try
 	{
-		Bureaucrat Harry("Harry", 45);
-		RobotomyRequestForm Rform("Harry");
-		Harry.signForm(Rform);
-		Harry.executeForm(Rform);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	std::cout << "\n------------------------\n\n";
-	try
-	{
-		Bureaucrat Mary("Mary", 137);
-		ShrubberyCreationForm Sform ("Mary");
-		Mary.signForm(Sform);
-		Mary.executeForm(Sform);
+		unknown_form = intern.makeForm("driving licence", "drive");
+		std::cout << *unknown_form << " was just created " << std::endl << std::endl;
 	}
 	catch (const std::exception& e)
 	{
-		std::cerr << e.what() << std::endl;
+		std::cout << e.what() << std::endl << std::endl;
 	}
-	std::cout << "\n------------------------\n\n";
-	try
-	{
-		Bureaucrat Lucas("Lucas", 1);
-		PresidentialPardonForm Pform("Lucas");
-		Lucas.signForm(Pform);
-		Lucas.executeForm(Pform);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	std::cout << "\n------------------------\n\n";
-	try
-	{
-		Bureaucrat Harry("Harry", 150);
-		RobotomyRequestForm Rform("Harry");
-		Harry.signForm(Rform);
-		Harry.executeForm(Rform);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	std::cout << "\n------------------------\n\n";
-	try
-	{
-		Bureaucrat Mary("Mary", 145);
-		ShrubberyCreationForm Sform ("Mary");
-		Mary.signForm(Sform);
-		Mary.executeForm(Sform);
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	std::cout << "DESTRUCTORS:" << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	delete robo_form;
+	delete shrub_form;
+	delete president_form;
+	delete unknown_form;
 	return (0);
+	//-------------------------------------------------------	
 }
